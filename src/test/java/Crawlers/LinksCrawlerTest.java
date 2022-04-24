ae2ef4ece39ad6e2f10829ae404ab3532d3f7022
Lé Me <31079238+emirgoran@users.noreply.github.com>
2022-04-24 15:09:57 +0200

    Create LinksCrawlerTest.java

---
+++ src/test/java/Crawlers/LinksCrawlerTest.java
@@ -0,0 +1,60 @@
+package Crawlers;
+
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class LinksCrawlerTest {
+
+    @Test
+    void getUrlsFromWebsite_noUrlsOnWebsite_returnsEmpty() {
+        Document document = new Document("localhost");
+
+        List<String> urls = LinksCrawler.GetUrlsFromWebsite(document);
+
+        Assertions.assertEquals(0, urls.size());
+    }
+
+    @Test
+    void getUrlsFromWebsite_testNormalCase_returnsListOfUrls() {
+        Document document = new Document("localhost");
+        document.append("<a href=\"http:\\\\www.test-website.com\"> Test website </a>");
+
+        List<String> urls = LinksCrawler.GetUrlsFromWebsite(document);
+
+        Assertions.assertEquals(1, urls.size());
+        Assertions.assertEquals("http:\\\\www.test-website.com", urls.get(0));
+    }
+
+    @Test
+    void getUrlsFromWebsite_testMaxNumberOfLinksExactly_returnsTheListOfUrls() {
+        Document document = new Document("localhost");
+        for (int i = 0; i < LinksCrawler.MAX_LINKED_WEBSITES; i++) {
+            document.append("<a href=\"http:\\\\www.test-website.com\"> Test website </a>");
+        }
+
+        List<String> urls = LinksCrawler.GetUrlsFromWebsite(document);
+
+        Assertions.assertEquals(LinksCrawler.MAX_LINKED_WEBSITES, urls.size());
+        for (int i = 0; i < LinksCrawler.MAX_LINKED_WEBSITES; i++) {
+            Assertions.assertEquals("http:\\\\www.test-website.com", urls.get(i));
+        }
+    }
+
+    @Test
+    void getUrlsFromWebsite_testMaxNumberOfLinksMoreThanAllowed_returnsReducedListOfUrls() {
+        Document document = new Document("localhost");
+        for (int i = 0; i < LinksCrawler.MAX_LINKED_WEBSITES + 1; i++) {
+            document.append("<a href=\"http:\\\\www.test-website.com\"> Test website </a>");
+        }
+
+        List<String> urls = LinksCrawler.GetUrlsFromWebsite(document);
+
+        Assertions.assertEquals(LinksCrawler.MAX_LINKED_WEBSITES, urls.size());
+        for (int i = 0; i < LinksCrawler.MAX_LINKED_WEBSITES; i++) {
+            Assertions.assertEquals("http:\\\\www.test-website.com", urls.get(i));
+        }
+    }
+}
