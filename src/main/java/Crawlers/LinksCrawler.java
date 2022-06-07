858971668eec954a6c23f5c4e68fbc0ab3539283
Lé Me <31079238+emirgoran@users.noreply.github.com>
2022-06-07 23:28:44 +0200

    Update LinksCrawler.java

--- src/main/java/Crawlers/LinksCrawler.java
+++ src/main/java/Crawlers/LinksCrawler.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class LinksCrawler {
 
+    // Attention: Increasing this constant's value will significantly increase runtime!
     public static final int MAX_LINKED_WEBSITES = 3;
 
     public static List<String> GetUrlsFromWebDocument(WebDocument webDocument) {
